cc38c868a078b17cc1271d929802d4380bb15baf
“Vitali <abdullajondev@gmail.com>
2023-01-15 23:41:28 +0500

    i have added new files

---
+++ corrency.py
@@ -0,0 +1,10 @@
+"""user_money_amount = input("please enter your money amount:  ")
+rate = 0.95
+euro = int(user_money_amount) * rate
+print(euro)"""
+
+ranking = ['John', 'Sen', 'Lisa']
+user_rank_number = int(input("please enter your rank number: ")) - 1
+
+user_rank = ranking[user_rank_number]
+print(user_rank)
