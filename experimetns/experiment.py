629774748ca6a1509905d38f16e2c8330cc931d9
“Vitali <abdullajondev@gmail.com>
2023-01-18 02:00:21 +0500

    some codes were changed
    Took 1 hour 15 minutes

--- experimetns/experiment.py
+++ experimetns/experiment.py
@@ -8,15 +8,34 @@ for content, filename in zip(contents, filenames):
     file = open(f"textfiles/{filename}", "w")
     file.write(content)"""
 
-
-file = open("textfiles/new.txt", "r")
+"""file = open("textfiles/new.txt", "r")
 words = file.readlines()
 
 for num, word in enumerate(words):
     upperwords = word.title()
     row = f"{num}-{upperwords}"
-    print(row)
+    print(row)"""
 
 """file = open("textfiles/new.txt", 'r')
 content = file.read()
 print(content.title())"""
+
+
+
+
+
+
+
+##LIST comprehension
+
+"""filenames = ['1.doc', '2.report', '3.presentation']
+
+filenames = [file.replace('.', '-') + ".txt" for file in filenames]
+print(filenames)"""
+
+names = ["john smith", "jay santi", "eva kuki"]
+
+names = [i.title() for i in names]
+print(names)
+
+
--- main.py
+++ main.py
@@ -7,32 +7,54 @@ while True:
         case 'add':
             todo = input("inter to do: ") + "\n"
 
-            file = open("files/todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            with open("files/todos.txt", "r") as file:
+                todos = file.readlines()
 
-            todos.append(todo)
+                todos.append(todo)
 
-            file = open("files/todos.txt", 'w')
-            file.writelines(todos)
-            file.close()
+            with open("files/todos.txt", 'w') as file:
+                file.writelines(todos)
+
         case 'show' | 'display':
-            file = open("files/todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            with open("files/todos.txt", "r") as file:
+                todos = file.readlines()
 
-            for index, item in enumerate(todos):
+            new_todos = []
+
+            for item in todos:
+                new_item = item.strip('\n')
+                new_todos.append(new_item)
+
+            for index, item in enumerate(new_todos):
                 item = item.title()
+                index = int(index + 1)
                 row = f"{index}-{item}"
                 print(row)
         case 'edit':
             number = int(input('please enter the number you wont to edit:  '))
             number = number - 1
-            new_todo = input("pls enter new todo")
-            todos[number] = new_todo
+
+            with open("files/todos.txt", "r") as file:
+                todos = file.readlines()
+
+            new_todo = input("pls enter new todo: ")
+            todos[number] = new_todo + "\n"
+
+            with open("files/todos.txt", "w") as file:
+                file.writelines(todos)
         case 'complete': 
             number = int(input("plase enter the task you complete: "))
-            todos.pop(number-1)
+            index = number - 1
+            with open("files/todos.txt", "r") as file:
+                todos = file.readlines()
+
+            todos.pop(index)
+            todo_completed = todos[index].strip('\n')
+
+            with open("files/todos.txt", "w") as file:
+                file.writelines(todos)
+            message = f"todo {todo_completed} was removed from the list"
+            print(message)
         case 'exit':
             break
         case _:
