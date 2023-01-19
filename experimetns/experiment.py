aa28f7e6886fe6c1d9c525ad5232e93bbdaa45fb
“Vitali <abdullajondev@gmail.com>
2023-01-19 23:16:25 +0500

    some prictise done

--- experimetns/experiment.py
+++ experimetns/experiment.py
@@ -33,9 +33,43 @@ print(content.title())"""
 filenames = [file.replace('.', '-') + ".txt" for file in filenames]
 print(filenames)"""
 
-names = ["john smith", "jay santi", "eva kuki"]
+"""names = ["john smith", "jay santi", "eva kuki"]
 
 names = [i.title() for i in names]
-print(names)
+print(names)"""
+
+
+
+"""date = input("Enter today's date: ")
+mood = input("how do you rate your mood today from 1 to 10: ")
+thoughts = input("let your thoughts flow: ")
+
+with open(f"textfiles/{date}.txt", "w") as file:
+    file.write(mood + "\n")
+    file.write(thoughts)"""
+
+"""while True:
+    with open("textfiles/sides.txt", 'r') as file:
+        sides = file.readlines()
+
+    side = input("Throw the coin and enter head or tail here: ?") + "\n"
+
+    sides.append(side)
+
+    with open("textfiles/sides.txt", 'w') as file:
+        file.writelines(sides)
+
+    nr_heads = sides.count("head\n")
+    percentage = nr_heads / len(sides) * 100
+
+    print(f"Heads: {percentage}%")"""
+while True:
+    with open("textfiles/file.txt", 'r') as file:
+        words = file.readlines()
+        user_input = input("say somthing: ")
+        words.append(user_input)
+
+    with open("textfiles/file.txt", "w") as file:
+        words = file.writelines(user_input)
 
 
