f98ddde4740c41ddc444a011de04e3035a441c96
“Vitali <abdullajondev@gmail.com>
2023-01-31 19:53:42 +0500

    added new file

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+from functions import get_todos, write_todos
 
-def get_todos(filepath):
-    with open(filepath, "r") as file:
-        todos = file.readlines()
-    return todos
-
-def write_todos(filepath, todos_arg):
-    with open(filepath, "w") as file:
-        file.writelines(todos_arg)
 
 while True:
     user_action = input("Type: add, show,complete, edit, exit:  ")
@@ -16,16 +9,16 @@ while True:
         try:
             todo = user_action[3:] + "\n"
 
-            todos = get_todos("files/todos.txt")
+            todos = get_todos()
 
             todos.append(todo)
 
-            write_todos("files/todos.txt", todos)
+            write_todos(todos)
         except ValueError:
             print("your commend is not valid")
             continue
     elif 'show' in user_action:
-        todos = get_todos("files/todos.txt")
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -36,12 +29,12 @@ while True:
             number = int(user_action[4:])
             number = number - 1
 
-            todos = get_todos("files/todos.txt")
+            todos = get_todos()
 
             new_todo = input("pls enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            write_todos("files/todos.txt", todos)
+            write_todos(todos)
         except ValueError:
             print("your commend is not valid")
     elif 'complete' in user_action:
@@ -49,13 +42,13 @@ while True:
             number = int(user_action[9:])
             index = number - 1
 
-            todos = get_todos("files/todos.txt")
+            todos = get_todos()
 
             todo_completed = todos[index].strip('\n')
             todos.pop(index)
 
 
-            write_todos("files/todos.txt", todos)
+            write_todos(todos)
             message = f"todo {todo_completed} was removed from the list"
             print(message)
         except IndexError:
