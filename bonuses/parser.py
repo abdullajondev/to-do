f4b0f2cffe55a22424245e03792811108ed6ed25
“Vitali <abdullajondev@gmail.com>
2023-02-06 15:16:09 +0500

    added new things

--- bonuses/parser.py
+++ bonuses/parser.py
@@ -1,8 +1,5 @@
-user_input = input("Enter feet and inches")
-
 def parse(feet_inches):
     parts = feet_inches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
-    
-
+    return {"feet": feet, "inches": inches}
